7a1e4d663848e7411b18761d49f65e0ca565528e
SravanthiVadla27 <sravanthi2706@gmail.com>
2025-11-13 00:15:08 +0530

    .

--- utils/expressError.js
+++
@@ -1,8 +0,0 @@
-class ExpressError extends Error {
-  constructor(status = 500, message = "Something went wrong!") {
-    super(message);
-    this.status = status;
-  }
-}
-
-module.exports = ExpressError;
